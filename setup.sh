0ab4d729b8fa3c2cb3ff49734236f1339ccd5695
chLi <565353780@qq.com>
2025-12-25 15:00:36 +0800

    add scikit-sparse install

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
-conda install -c conda-forge suitesparse -y
+conda install -c conda-forge scikit-sparse -y
 
 pip3 install torch torchvision \
   --index-url https://download.pytorch.org/whl/cu128
 
-pip install -U open3d numpy scipy tqdm scikit-image \
-  scikit-learn scikit-sparse tensorboard opencv-python \
-  trimesh ninja
+pip install open3d numpy scipy tqdm scikit-image \
+  scikit-learn tensorboard opencv-python trimesh \
+  ninja
 
 pip install moviepy==1.0.3
