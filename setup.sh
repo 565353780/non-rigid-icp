0d1d2af34df7ddd15173fa72341c81d05a1c39e6
chLi <565353780@qq.com>
2026-02-06 16:54:27 +0800

    adaptive to current gpu

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 conda install -c conda-forge scikit-sparse -y
 
+pip install ninja
+
 pip3 install torch torchvision \
-  --index-url https://download.pytorch.org/whl/cu128
+  --index-url https://download.pytorch.org/whl/cu124
 
 pip install open3d numpy scipy tqdm scikit-image \
-  scikit-learn tensorboard opencv-python trimesh \
-  ninja
+  scikit-learn tensorboard opencv-python trimesh
 
 pip install moviepy==1.0.3
